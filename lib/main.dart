6baa8e585c553e8660d5276b6cc27ccfc7083476
Luis David Garcia Lunar <ldgl1215@gmail.com>
2021-11-06 10:50:25 -0300

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  //test
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
